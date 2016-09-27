49961d46de5eb2ad43551af514659a79809244cc
mguimond5 <m.guimond3@sjasd.ca>
2016-09-27 10:42:57 -0500

    Assignment for Mr.Hardman's class
    Assignment for CS20S Emotions Challenge

---
+++ Emotions Assignment/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Emotions_Assignment
+{
+    partial class fr1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.bt3 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(12, 13);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "Happy";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(12, 77);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(75, 23);
+            this.bt2.TabIndex = 1;
+            this.bt2.Text = "Sad";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(153, 13);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(75, 23);
+            this.bt4.TabIndex = 3;
+            this.bt4.Text = "Mad";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(153, 77);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(75, 23);
+            this.bt5.TabIndex = 4;
+            this.bt5.Text = "Tired";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Location = new System.Drawing.Point(153, 158);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(75, 23);
+            this.bt6.TabIndex = 5;
+            this.bt6.Text = "Bored";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(12, 158);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(75, 23);
+            this.bt3.TabIndex = 2;
+            this.bt3.Text = "Shy";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(13, 214);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 6;
+            // 
+            // fr1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Name = "fr1";
+            this.Text = "Select Your Emotion";
+            this.Load += new System.EventHandler(this.fr1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Emotions Assignment/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emotions_Assignment
+{
+    public partial class fr1 : Form
+    {
+        public fr1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Make sure Mr.Hardman doesn't yell at you!";
+        }
+
+        private void fr1_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Mr.Hardman yelled at you didn't he...";
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Be the best you can be!";
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "It's Friday!";
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Sleep later, work now.";
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Watch 'How to get away with Murder' on Netflix.";
+        }
+    }
+}
